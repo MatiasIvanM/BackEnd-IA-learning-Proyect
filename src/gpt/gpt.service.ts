aeb9001ca61461d5a9a7b05d41178ce16e4ea054
MatiasIvanM <matiasivan.sd@gmail.com>
2024-06-27 15:35:00 -0300

    OpenAi conections & orthography

--- src/gpt/gpt.service.ts
+++ src/gpt/gpt.service.ts
@@ -1,15 +1,19 @@
-import { Injectable } from '@nestjs/common';
-import { orthographyChecUseCase } from './use-cases';
-import { orthographyDto } from './dtos/orthography.dto';
+import { Injectable } from "@nestjs/common";
+import { orthographyChecUseCase } from "./use-cases";
+import { orthographyDto } from "./dtos/orthography.dto";
+import OpenAI from "openai";
 
 @Injectable()
 export class GptService {
+  private openai = new OpenAI({
+    apiKey: process.env.OPENAI_API_KEY,
+  });
 
-// Solo va a llamar casos de uso
+  // Solo va a llamar casos de uso
 
-    async orthographyCheck(orthographyDto : orthographyDto){
-        return await orthographyChecUseCase({
-            prompt: orthographyDto.prompt
-        }); 
-    }
+  async orthographyCheck(orthographyDto: orthographyDto) {
+    return await orthographyChecUseCase(this.openai, {
+      prompt: orthographyDto.prompt,
+    });
+  }
 }
--- src/gpt/use-cases/orthography.use-case.ts
+++ src/gpt/use-cases/orthography.use-case.ts
@@ -1,17 +1,54 @@
+import OpenAI from "openai";
 
 interface Options {
-    prompt: string;
-
+  prompt: string;
 }
 
+export const orthographyChecUseCase = async (
+  openai: OpenAI,
+  options: Options
+) => {
+  const { prompt } = options;
 
-export const orthographyChecUseCase = async(options : Options )=> {
+  const completion = await openai.chat.completions.create({
+    messages: [
+      {
+        role: "system",
+        content: `Te seran proveídos texto en español con posibles errores ortogrpáficos y gramaticales,
+        Las palabras usadas deben de existir en el español, no se permiten palabras inventadas.
+        Debes responder en formato JSON,
+        Tu tarea es corregir y retornar información de las soluciones y el texto original con sus correcciones,
+        También debes de dar un porcentaje de aciertos por el usuario,
 
-    const { prompt } = options;
-
-    return {
-        prompt: prompt,
         
-    };
+        
+        
+        Si no hay errreos, debes retornar un mensaje de felicitaciones.
+        
+        Ejemplo de salida:
+        {
+            userScore: number,
+            errors:string[], // ['error -> solución']
+            prompCorregida:string
+            message: string, // Usa emojis y texto para felicitar al usuario
+        }
 
+        `, //comportamiento inicial
+      },
+      {
+        role: "user",
+        content: prompt,
+      },
+    ],
+    model: "gpt-4o",
+    temperature: 0.3,
+    max_tokens: 100,
+    // Formatea las trspuestas y asegura de que siempre sean json pero no todos los modelos soportan esta forma de enviar las respuestas 
+    // response_format: {
+    //     type: "json_object"
+    // }
+  });
+//   console.log("🚀 ~ completion:", completion);
+
+  return JSON.parse(completion.choices[0].message.content);
 };
